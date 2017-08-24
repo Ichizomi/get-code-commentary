abeee68359fa3733e0b5c5a6d4325b50df41526f
Ichizomi <rksgrijo@gmail.com>
2017-08-24 07:56:29 -0400

    Updated

--- get-php-comments.sh
+++ get-php-comments.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-# This script saves all javascript comments from 
-#an input .js file to and output text file!
+# This script saves all php comments from 
+#an input .php file to and output text file!
 # Instructions:
-# 1) - place the input .js file on the same folder as this script.
-# 2) - run this script via terminal and send the file name.js as first argument.
+# 1) - place the input .php file on the same folder as this script.
+# 2) - run this script via terminal and send the file name.php as first argument.
 # 		send name.txt as second argument for output
 inputfile=$1
 outputfile=$2
